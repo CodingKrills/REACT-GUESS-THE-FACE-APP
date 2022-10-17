8e333c644f3eee1cf696bf903800391b96b3631d
codingkrills <pulkitsinghmail@gmail.com>
2022-10-17 16:53:27 +0530

    updates

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,55 +1,69 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 
+import confused from '../public/icons/confused.png'
+
 const Dashboard = () => {
     return (
         <div className='container' style={{ marginTop: "4%" }}>
+
+            <div class="px-4 py-4 my-4 text-center">
+                <img class="d-block mx-auto mb-4" src={confused} alt="" width="72" height="57" />
+                <h1 class="display-5 fw-bold">GUESS THE FACE</h1>
+                <div class="col-lg-6 mx-auto">
+                    {/* <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s most popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system, extensive prebuilt components, and powerful JavaScript plugins.</p> */}
+                    {/* <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
+                        <button type="button" class="btn btn-primary btn-lg px-4 gap-3">Primary button</button>
+                        <button type="button" class="btn btn-outline-secondary btn-lg px-4">Secondary</button>
+                    </div> */}
+                </div>
+            </div>
 
             {/* //* ROW */}
             <div className='row'>
 
                 {/* //* COL */}
                 <div className='col-3'>
-                    <div className="card shadow">
-                        <div className="card-body text-center">
-                            <Link className='nav-link' to='person/1/step/1'>
+                    <Link className='nav-link' to='person/1/step/1'>
+                        <div className="card shadow py-3">
+                            <div className="card-body text-center">
                                 <b>GUESS THE PERSON NO 1</b>
-                            </Link>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
 
                 {/* //* COL */}
                 <div className='col-3'>
-                    <div className="card shadow">
-                        <div className="card-body text-center">
-                            <Link className='nav-link' to='person/2/step/2'>
+                    <Link className='nav-link' to='person/2/step/2'>
+                        <div className="card shadow py-3">
+                            <div className="card-body text-center">
                                 <b>GUESS THE PERSON NO 2</b>
-                            </Link>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
 
                 {/* //* COL */}
                 <div className='col-3'>
-                    <div className="card shadow">
-                        <div className="card-body text-center">
-                            <Link className='nav-link' to='person/3/step/3'>
+                    <Link className='nav-link' to='person/3/step/3'>
+                        <div className="card shadow py-3">
+                            <div className="card-body text-center">
                                 <b>GUESS THE PERSON NO 3</b>
-                            </Link>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
 
                 {/* //* COL */}
                 <div className='col-3'>
-                    <div className="card shadow">
-                        <div className="card-body text-center">
-                            <Link className='nav-link' to='person/4/step/4'>
+                    <Link className='nav-link' to='person/4/step/4'>
+                        <div className="card shadow py-3">
+                            <div className="card-body text-center">
                                 <b>GUESS THE PERSON NO 4</b>
-                            </Link>
+                            </div>
                         </div>
-                    </div>
+                    </Link>
                 </div>
 
             </div>
--- src/pages/Person.js
+++ src/pages/Person.js
@@ -9,6 +9,8 @@ import '../public/css/overlay.css'
 // import person1step2 from '../public/persons/virat-kohli/person1step2.png'
 // import person1step3 from '../public/persons/virat-kohli/person1step3.png'
 // import person1step4 from '../public/persons/virat-kohli/person1step4.png'
+// import home from '../public/icons/home.png'
+
 import person1 from '../public/persons/person1.png'
 
 import person2 from '../public/persons/person2.jpg'
@@ -17,24 +19,29 @@ const Person = () => {
 
     const { personId, stepId } = useParams();
 
-    const [pId, setPid] = useState(2);
-    const [sId, setSid] = useState(1);
+    const [pId, setPid] = useState(null);
+    const [sId, setSid] = useState(null);
 
     console.log(pId, sId)
 
     useEffect(() => {
+
         setPid(Number(personId))
-        setPid(Number(stepId))
-    }, [])
+        setSid(Number(stepId))
+
+    }, [personId, stepId])
 
     return (
         <div className='container' style={{ marginTop: "1%" }}>
 
-            <div className='text-center'>
+            <div className='row justify-content-left'>
+                <Link to="/">
+                    <h6>GO HOME</h6>
+                </Link>
+            </div>
+
+            <div className=''>
                 <div className='row justify-content-center'>
-                    <Link to="/">
-                        <h6>GO HOME</h6>
-                    </Link>
                     <div className='col col-md-1'>
                         <h6>PERSON - <b>{pId}</b></h6>
                     </div>
@@ -50,40 +57,40 @@ const Person = () => {
                 {pId === 1 && sId === 1 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person1} alt="virat" />
-                        <div class="overlay-top-step-1"></div>
-                        <div class="overlay-right-step-1"></div>
-                        <div class="overlay-bottom-step-1"></div>
-                        <div class="overlay-left-step-1"></div>
+                        <div className="overlay-top-step-1"></div>
+                        <div className="overlay-right-step-1"></div>
+                        <div className="overlay-bottom-step-1"></div>
+                        <div className="overlay-left-step-1"></div>
                     </div>
                 }
 
                 {pId === 1 && sId === 2 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person1} alt="virat" />
-                        <div class="overlay-top-step-2"></div>
-                        <div class="overlay-right-step-2"></div>
-                        <div class="overlay-bottom-step-2"></div>
-                        <div class="overlay-left-step-2"></div>
+                        <div className="overlay-top-step-2"></div>
+                        <div className="overlay-right-step-2"></div>
+                        <div className="overlay-bottom-step-2"></div>
+                        <div className="overlay-left-step-2"></div>
                     </div>
                 }
 
                 {pId === 1 && sId === 3 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person1} alt="virat" />
-                        <div class="overlay-top-step-3"></div>
-                        <div class="overlay-right-step-3"></div>
-                        <div class="overlay-bottom-step-3"></div>
-                        <div class="overlay-left-step-3"></div>
+                        <div className="overlay-top-step-3"></div>
+                        <div className="overlay-right-step-3"></div>
+                        <div className="overlay-bottom-step-3"></div>
+                        <div className="overlay-left-step-3"></div>
                     </div>
                 }
 
                 {pId === 1 && sId === 4 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person1} alt="virat" />
-                        <div class="overlay-top-step-4"></div>
-                        <div class="overlay-right-step-4"></div>
-                        <div class="overlay-bottom-step-4"></div>
-                        <div class="overlay-left-step-4"></div>
+                        <div className="overlay-top-step-4"></div>
+                        <div className="overlay-right-step-4"></div>
+                        <div className="overlay-bottom-step-4"></div>
+                        <div className="overlay-left-step-4"></div>
                     </div>
                 }
 
@@ -94,40 +101,40 @@ const Person = () => {
                 {pId === 2 && sId === 1 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person2} alt="virat" />
-                        <div class="overlay-top-step-1"></div>
-                        <div class="overlay-right-step-1"></div>
-                        <div class="overlay-bottom-step-1"></div>
-                        <div class="overlay-left-step-1"></div>
+                        <div className="overlay-top-step-1"></div>
+                        <div className="overlay-right-step-1"></div>
+                        <div className="overlay-bottom-step-1"></div>
+                        <div className="overlay-left-step-1"></div>
                     </div>
                 }
 
                 {pId === 2 && sId === 2 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person2} alt="virat" />
-                        <div class="overlay-top-step-2"></div>
-                        <div class="overlay-right-step-2"></div>
-                        <div class="overlay-bottom-step-2"></div>
-                        <div class="overlay-left-step-2"></div>
+                        <div className="overlay-top-step-2"></div>
+                        <div className="overlay-right-step-2"></div>
+                        <div className="overlay-bottom-step-2"></div>
+                        <div className="overlay-left-step-2"></div>
                     </div>
                 }
 
                 {pId === 2 && sId === 3 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person2} alt="virat" />
-                        <div class="overlay-top-step-3"></div>
-                        <div class="overlay-right-step-3"></div>
-                        <div class="overlay-bottom-step-3"></div>
-                        <div class="overlay-left-step-3"></div>
+                        <div className="overlay-top-step-3"></div>
+                        <div className="overlay-right-step-3"></div>
+                        <div className="overlay-bottom-step-3"></div>
+                        <div className="overlay-left-step-3"></div>
                     </div>
                 }
 
                 {pId === 2 && sId === 4 &&
                     <div className='image-container'>
                         <img className='img-fluid overlay-image-responsive' width={500} height={500} src={person2} alt="virat" />
-                        <div class="overlay-top-step-4"></div>
-                        <div class="overlay-right-step-4"></div>
-                        <div class="overlay-bottom-step-4"></div>
-                        <div class="overlay-left-step-4"></div>
+                        <div className="overlay-top-step-4"></div>
+                        <div className="overlay-right-step-4"></div>
+                        <div className="overlay-bottom-step-4"></div>
+                        <div className="overlay-left-step-4"></div>
                     </div>
                 }
 
@@ -139,7 +146,7 @@ const Person = () => {
                             <button
                                 className={`
                              btn btn-lg 
-                         ${sId === 1 ? 'btn-primary ' : 'btn-outline-primary disabled'}
+                         ${sId === 1 && pId !== 1 ? 'btn-primary ' : 'btn-outline-primary disabled'}
                          `}
                                 onClick={() => { setPid(pId - 1); setSid(1) }}>
                                 PREV PERSON
